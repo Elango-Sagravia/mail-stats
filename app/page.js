04e5be164b689b291710fa8b1d5d0252a26382c2
Elango-Sagravia <elango@sagravia.com>
2024-10-17 14:42:16 +0530

    initial setup

---
+++ app/page.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import {
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  ShoppingCart,
+  Users,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+export const description =
+  "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action.";
+
+function Homepage() {
+  return <div>dashboard</div>;
+}
+
+export default Homepage;
